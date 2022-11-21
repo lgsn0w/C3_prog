ed49b4dc51f3114838771f968ea6bc835ece5706
lgsn0w <112468011+lgsn0w@users.noreply.github.com>
2022-11-21 16:06:37 -0300

    Update RegAgenda.java

--- RegAgenda.java
+++ RegAgenda.java
@@ -1,24 +1,43 @@
-package C3;
+package InsercaoSequencial;
 
-public class RegAgenda {
+public class DB {
     private String nome;
-    private String end;
-    private String tel;
+    private String nome_jogo;
+    private String ano;
 
 ///Constructor
-    public RegAgenda(String nome, String end, String tel) {
+    public DB(String nome, String nome_jogo, String ano) {
         this.nome = nome;
-        this.end = end;
-        this.tel = tel;
+        this.nome_jogo = nome_jogo;
+        this.ano = ano;
     }
 ///Functions
     public String mostraNome(){
         return nome;
     }
-    public String mostraEnd(){
-        return end;
+    public String mostraNomeJogo(){
+        return nome_jogo;
     }
-    public String mostraTel(){
-        return tel;
+    public String mostraAno(){
+        return ano;
+    }
+
+    public void alteraNomeJogo(String nomeJogo){
+        this.nome_jogo = nomeJogo;
+    }
+    public void alterarAno(String ano){
+        this.ano = ano;
+    }
+    public void alterarNome(String nome){
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "RegAgenda{" +
+                "nome='" + nome + '\'' +
+                ", end='" + nome_jogo + '\'' +
+                ", tel='" + ano + '\'' +
+                '}';
     }
 }
